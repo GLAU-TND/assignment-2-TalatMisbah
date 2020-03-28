e56ec9b9b38073afd6670caaaaaf617d8002be75
talat <talat.misbah_cs18@gla.ac.in>
2020-03-28 20:25:07 +0530

    defined main of problem 4

---
+++ src/problem4/node/Node.java
@@ -0,0 +1,28 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 23-Mar-20
+ *  Time: 7:13 PM
+ */
+package problem4.node;
+
+public class Node
+{
+    int data;
+    Node next;
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
