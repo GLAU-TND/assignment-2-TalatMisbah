7ce305185e2eeca7cb7fa3fbafe95811e7c30a18
talat <talat.misbah_cs18@gla.ac.in>
2020-03-28 20:26:43 +0530

    Added preOrder and postOrder function in MyBinarySearchTree

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,22 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String args[]) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(20);
+        obj.insert(24);
+        obj.insert(15);
+        obj.insert(17);
+        obj.insert(19);
+        obj.insert(22);
+        obj.insert(39);
+        obj.insert(60);
+        System.out.print("Left Child nodes are: ");
+        obj.printLeftNode(obj.root);
+        System.out.println();
+        System.out.println("No of nodes that dosn't have a left child node are: "+ obj.getCount());
+    }
 }
--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,25 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String args[]) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(3);
+        obj.insert(2);
+        obj.insert(8);
+        obj.insert(5);
+        obj.insert(7);
+        obj.insert(10);
+        obj.insert(13);
+        obj.preOrder(obj.root);
+        System.out.println();
+        obj.postOrder(obj.root);
+        System.out.println("\nFirst statement is true");
+        System.out.println("Second statement is false as we didn't get the middle point same.");
+    }
 }
