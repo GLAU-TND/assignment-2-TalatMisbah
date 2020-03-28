9f1e8c47e4b98c4ae11237639107419b38a34d08
talat <talat.misbah_cs18@gla.ac.in>
2020-03-28 20:25:59 +0530

    Defined node class using the object of student from problem 5

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String args[]) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Alina",11);
+        obj.insert("Talat",52);
+        obj.insert("Kumud",14);
+        obj.insert("Ayushii",2);
+        obj.insert("Gauri",58);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
